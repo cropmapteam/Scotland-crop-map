5bb97b2e4279b9da688f6537f8c170daa935238c
James Crone <james.crone@ed.ac.uk>
2019-07-25 12:47:55 +0100

    added comments

--- zonal_experiments/create_file_structure_for_cnn.py
+++ zonal_experiments/create_file_structure_for_cnn.py
@@ -1,3 +1,25 @@
+"""
+   as input for the CNN ml, beata needed the set of images produced by
+   resize_clipped_images_to_64by64.py greyscaled and then the images
+   placed into a Test/Train directory structure specifically required
+   by the R CNN ml process. Beata did the greyscaling herself. What
+   this script does is take the folder of greyscaled images and creates
+   and moves the images into the Test/Train directory structure
+
+   Under each Test/Train folder is a sub-folder for every LCTYPE which
+   contains all the images of that LCTYPE
+
+   there is a 60/40 Test/Train split
+
+   if there are less than 4 instances of a particular LCTYPE, the LCTYPE is
+   skipped (because 2 in Test/Train in such cases is not enough samples
+
+   data/kelso_image_clips_LUT.csv gives the labels of each image, given:
+
+   S1B_20180922_30_asc_175817_175842_DV_Gamma-0_GB_OSGB_RCTK_SpkRL_450_resized.tif
+
+   450 is an id which there is a record for in data/kelso_image_clips_LUT.csv
+"""
 import csv
 import glob
 import os
@@ -65,8 +87,8 @@ def create_file_structure(base_path_to_images_to_be_moved, base_output_path):
 
 def main():
     create_file_structure(
-        base_path_to_images_to_be_moved="/home/james/geocrud/Clipped/ValidResized",
-        base_output_path="/home/james/geocrud/BeataData"
+        base_path_to_images_to_be_moved="/home/james/Desktop/KelsoGreyscale",
+        base_output_path="/home/james/Desktop/KelsoGreyscaleRestructured"
     )
 
 
