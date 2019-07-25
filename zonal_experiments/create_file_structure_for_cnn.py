db8d53f2927ef9874ce9916c038b30eb92a9fda4
James Crone <james.crone@ed.ac.uk>
2019-07-25 10:21:05 +0100

    initial commit - this takes the 64x64 images beata has greyscaled and creates/moves the images into the test/train directory structure required for the CNN ml

---
+++ zonal_experiments/create_file_structure_for_cnn.py
@@ -0,0 +1,76 @@
+import csv
+import glob
+import os
+import shutil
+
+
+def create_file_structure(base_path_to_images_to_be_moved, base_output_path):
+    my_d = {}
+
+    with open("data/kelso_image_clips_LUT.csv", "r") as inpf:
+        my_reader = csv.DictReader(inpf)
+        for r in my_reader:
+            lctype = r["lctype"]
+            gid = r["gid"]
+            if lctype in my_d:
+                my_d[lctype].append(gid)
+            else:
+                my_d[lctype] = [gid]
+
+    if not os.path.exists(os.path.join(base_output_path, "Test")):
+        os.makedirs(os.path.join(base_output_path, "Test"))
+
+    if not os.path.exists(os.path.join(base_output_path, "Train")):
+        os.makedirs(os.path.join(base_output_path, "Train"))
+
+    for lctype in my_d:
+        instance_gids = my_d[lctype]
+        num_instances = len(instance_gids)
+        if num_instances < 4:
+            # there needs to be at least 4 instances of a feature, otherwise the feature is skipped
+            print("Too few instances for {}".format(lctype))
+        else:
+            print("Number of instances for {} is {}".format(lctype, num_instances))
+
+            split_point = int(round((num_instances / 100.0) * 60))
+            idx = 1
+            test_or_train_type = None
+            for i in instance_gids:
+                if idx <= split_point:
+                    test_or_train_type = "Test"
+                else:
+                    test_or_train_type = "Train"
+                idx += 1
+
+                #images_to_move_path = "/home/james/geocrud/Clipped/ValidResized/*_SpkRL_{}_*.tif".format(str(i))
+
+                images_to_move_path = (os.path.join(base_path_to_images_to_be_moved, "*_SpkRL_{}_*.tif")).format(
+                    str(i)
+                )
+
+                images_to_move = glob.glob(images_to_move_path)
+                num_images_to_move = len(images_to_move)
+
+                for img_to_move in images_to_move:
+                    img_to_move_dst = os.path.join(
+                        base_output_path,
+                        test_or_train_type,
+                        lctype,
+                        os.path.split(img_to_move)[-1]
+                    )
+
+                    if not os.path.exists(os.path.join(base_output_path, test_or_train_type, lctype)):
+                        os.makedirs(os.path.join(base_output_path, test_or_train_type, lctype))
+
+                    shutil.copyfile(img_to_move, img_to_move_dst)
+
+
+def main():
+    create_file_structure(
+        base_path_to_images_to_be_moved="/home/james/geocrud/Clipped/ValidResized",
+        base_output_path="/home/james/geocrud/BeataData"
+    )
+
+
+if __name__ == "__main__":
+    main()
