75863fe0041c02d0146870ab46801463996077bd
James Crone <james.crone@ed.ac.uk>
2019-07-30 09:56:47 +0100

    edited for epcc vm usage

--- zonal_experiments/mp_py_zonal_stats.py
+++ zonal_experiments/mp_py_zonal_stats.py
@@ -18,20 +18,22 @@ from py_zonal_stats import mp_fetch_zonal_stats_for_shapefile
 def main():
     jobs = []
     # is the folder containing all of the shapefiles we want to generate zonal stats for
-    path_to_shps = "/home/james/geocrud/partitions/*.shp"
+    path_to_shps = "/home/geojamesc/partitions/*.shp"
 
     # is a CSV describing which S1 images zonal stats are to found for
     image_metadata_fname = "data/image_bounds_meta.csv"
 
     # is where the output zonal stats csv`s should be placed
-    output_path = "/home/james/geocrud/zonal_stats"
+    output_path = "/home/geojamesc/geocrud/zonal_stats/mpzs"
 
     # assemble jobs list: [[zones_shp_fname, image_metadata_fname, output_path], ...]
     # each item in the list is a new job that will processed
     for zones_shp_fname in glob.glob(path_to_shps):
         jobs.append([zones_shp_fname, image_metadata_fname, output_path])
 
-    pool = Pool()
+    # processed defaults to os.cpu_count
+    # the epcc vm has 16 cores so set to 12
+    pool = Pool(processes=12)
     pool.map(mp_fetch_zonal_stats_for_shapefile, jobs)
 
 
