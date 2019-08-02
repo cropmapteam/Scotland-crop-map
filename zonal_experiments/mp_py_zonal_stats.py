02d63d9e5f4e4a7b42de1fdda720177eb21010da
James Crone <james.crone@ed.ac.uk>
2019-08-02 11:13:04 +0100

    replaced hardcoded path with cli args using click

--- zonal_experiments/mp_py_zonal_stats.py
+++ zonal_experiments/mp_py_zonal_stats.py
@@ -1,30 +1,30 @@
 """
-use python multiprocessing to process zonal stats in parallel
-using blocks of partitioned zonal shapefiles
-
-on my local pc, spread across 4 cores, time reported to create
-zonal stats from 16 copies of the kelso 413 poly shapefiles was
-
-real	9m56.298s
-user	37m21.889s
-sys	0m33.066s
+    use python multiprocessing to process zonal stats in parallel
+    using blocks of partitioned zonal shapefiles
 """
 import glob
+import os
 from multiprocessing import Pool
+import click
 # is our python stuff to generate the zonal stats
 from py_zonal_stats import mp_fetch_zonal_stats_for_shapefile
 
 
-def main():
+@click.command()
+@click.argument('path_to_shapefiles', type=click.Path(exists=True))
+@click.argument('image_metadata_fname', type=click.Path(exists=True))
+@click.argument('output_path', type=click.Path(exists=True))
+@click.argument('num_of_cores', type=click.IntRange(min=1, max=os.cpu_count()))
+def fetch_zonal_stats_for_shapefiles(path_to_shapefiles, image_metadata_fname, output_path, num_of_cores):
     jobs = []
     # is the folder containing all of the shapefiles we want to generate zonal stats for
-    path_to_shps = "/home/geojamesc/partitions/*.shp"
+    path_to_shps = os.path.join(path_to_shapefiles, "*.shp")
 
     # is a CSV describing which S1 images zonal stats are to found for
-    image_metadata_fname = "data/image_bounds_meta.csv"
+    #image_metadata_fname = "data/image_bounds_meta.csv"
 
     # is where the output zonal stats csv`s should be placed
-    output_path = "/home/geojamesc/geocrud/zonal_stats/mpzs"
+    #output_path = "/home/geojamesc/geocrud/zonal_stats/mpzs"
 
     # assemble jobs list: [[zones_shp_fname, image_metadata_fname, output_path], ...]
     # each item in the list is a new job that will processed
@@ -33,9 +33,9 @@ def main():
 
     # processed defaults to os.cpu_count
     # the epcc vm has 16 cores so set to 12
-    pool = Pool(processes=12)
+    pool = Pool(processes=num_of_cores)
     pool.map(mp_fetch_zonal_stats_for_shapefile, jobs)
 
 
 if __name__ == "__main__":
-    main()
+    fetch_zonal_stats_for_shapefiles()
