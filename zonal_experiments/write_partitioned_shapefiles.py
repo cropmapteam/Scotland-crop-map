4600c00762629e2f400b2fbfd0c374b1fd7d5e12
James Crone <james.crone@ed.ac.uk>
2019-08-06 09:59:02 +0100

    removed local Pg db conn string

--- zonal_experiments/write_partitioned_shapefiles.py
+++ zonal_experiments/write_partitioned_shapefiles.py
@@ -6,12 +6,10 @@ from postgres import Postgres
 import subprocess
 
 
-def get_partition_tile_names():
+def get_partition_tile_names(pg_conn_str):
     partition_tile_names = []
-
-    pg_conn_str = "postgres://james:MopMetal3@localhost:5432/cropmaps"
     db = Postgres(pg_conn_str)
-    sql = "SELECT distinct(tile_name) FROM zonal.scotland_full_training_w_os10kmsq"
+    sql = "SELECT distinct(tile_name) FROM zonal.scotland_full_lpis_w_os10kmsq"
     rs = db.all(sql)
     for r in rs:
         partition_tile_names.append(r)
@@ -19,11 +17,12 @@ def get_partition_tile_names():
     return partition_tile_names
 
 
-tile_names = get_partition_tile_names()
+pg_conn_str=""
+tile_names = get_partition_tile_names(pg_conn_str)
 
 for tn in tile_names:
     # we need -r to ensure postgis gid`s are included in the output
-    cmd = "pgsql2shp -r -f /home/james/geocrud/partitions/scotland_full_training_{0}.shp -h localhost -u james -P MopMetal3 cropmaps @SELECT * FROM zonal.scotland_full_training_w_os10kmsq WHERE tile_name='{1}'@".format(
+    cmd = "pgsql2shp -r -f /home/james/geocrud/partitions/scotland_full_lpis_{0}.shp -h localhost -u james -P MopMetal3 cropmaps @SELECT * FROM zonal.scotland_full_lpis_w_os10kmsq WHERE tile_name='{1}'@".format(
         tn.lower(), tn)
     cmd = cmd.replace('@','"')
     subprocess.call(cmd, shell=True)
