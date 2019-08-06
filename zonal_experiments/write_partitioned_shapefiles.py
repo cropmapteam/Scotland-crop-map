14649ee49f2c849b67a0ba256e4b80723ece09ed
James Crone <james.crone@ed.ac.uk>
2019-08-06 10:06:09 +0100

    removed harcoded db pwd

--- zonal_experiments/write_partitioned_shapefiles.py
+++ zonal_experiments/write_partitioned_shapefiles.py
@@ -21,8 +21,11 @@ pg_conn_str=""
 tile_names = get_partition_tile_names(pg_conn_str)
 
 for tn in tile_names:
+    db_host = ""
+    db_user = ""
+    db_pwd = ""
     # we need -r to ensure postgis gid`s are included in the output
-    cmd = "pgsql2shp -r -f /home/james/geocrud/partitions/scotland_full_lpis_{0}.shp -h localhost -u james -P MopMetal3 cropmaps @SELECT * FROM zonal.scotland_full_lpis_w_os10kmsq WHERE tile_name='{1}'@".format(
-        tn.lower(), tn)
-    cmd = cmd.replace('@','"')
+    cmd = "pgsql2shp -r -f /home/james/geocrud/partitions/scotland_full_lpis_{0}.shp -h {2} -u {3} -P {4} cropmaps @SELECT * FROM zonal.scotland_full_lpis_w_os10kmsq WHERE tile_name='{1}'@".format(
+        tn.lower(), tn, db_host, db_user, db_pwd)
+    cmd = cmd.replace('@', '"')
     subprocess.call(cmd, shell=True)
